1ab302d649fcd2eb4229934965873c612627f6b6
hosseinii-sanaa <hosseinii.sanaa@gmail.com>
2025-04-18 20:28:35 +0330

    Update doc.R

--- R/doc.R
+++ R/doc.R
@@ -4,9 +4,19 @@
 #'
 #' @format A data frame with 101 rows and 97 variables:
 #' \describe{
-#'   \item{column1}{Description of column1}
-#'   \item{column2}{Description of column2}
-#'   \item{...}{...}
+#'   \item{NobatAmargiri}{Survey Round}
+#'   \item{F2Kol_DJAYGOZIN}{Has the sample been updated with new households?}
+#'   \item{F2_D01}{row number}
+#'   \item{F2_D03}{Relationship with the household head}
+#'   \item{F2_D04}{sex}
+#'   \item{F2_D05}{birthdaydate-month}
+#'   \item{F2_D06}{birthdaydata-year}
+#'   \item{F2_D07}{age}
+#'   \item{F2_D08}{nationality}
+#'   \item{F2_D09}{Residence status}
+#'   \item{F2_D10}{Duration of continuous stay at this place}
+#'   \item{F2_D11}{Previous Residence location}
+#'   \item{F2_D12}{rownumber}
 #' }
 #'
 #' @source Source of the data (e.g. "Collected from [amar.org], original .mdb file")
